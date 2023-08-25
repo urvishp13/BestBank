4cd7053275b44b67c075d87c65b2243f9340c848
Picasso <urvishp13@gmail.com>
2023-08-25 15:56:41 -0400

    got spending bars' widths to display in an x^2 function fashion

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -9,7 +9,7 @@ let html = ''
 for (const account of accounts) {
     html += `
         <button class="btn info-btn" id="${account.id}">
-            <span>${account.title}</span> <span>${account.balance}</span>
+            <span class="title">${account.title}</span> <span class="balance">${account.balance}</span>
         </button>
     `
 }
@@ -17,6 +17,45 @@ for (const account of accounts) {
 accountsEl.innerHTML += html
 
 // grab the Spendings section of the page
+const spendingsEl = get("spendings")
 // populate the section with bars from the accounts.js file
     // everytime account is clicked, loop through each spending for the 
-    //      account and draw it to the DOM
+    // account and draw it to the DOM
+html = ''
+let spendingBarNum = 0
+accountsEl.querySelectorAll("button")
+    .forEach(accountBtn => accountBtn.addEventListener("click", function() {
+        const account = accounts[accountBtn.id-1]
+
+        for (const spending of account.spendings) {
+            html += `
+                <div id="${spendingBarNum++}" class="spendingBar">
+                    <span class="category">${spending.category}</span> <span class="spent">${spending.spent}</span>
+                </div>
+            `
+        }
+
+        spendingsEl.innerHTML += html
+
+        setWidthOfBars(document.getElementsByClassName("spendingBar"), account)
+        // reset html to prepare for displaying the spending for the next
+        //      accountBtn click
+        html = ''
+    }))
+
+const setWidthOfBars = (spendingBars, account) => {
+    let maxSpent = 0
+
+    for (const spending of account.spendings) {
+        const spent = Number(spending.spent)
+
+        if (maxSpent < spent) maxSpent = spent
+    }
+
+    for (const spendingBar of spendingBars) {
+        spendingBar.style.width = `calc( ${ mathFunction( spendingBars.length - spendingBar.id ) }em + 25%)`
+    }
+        
+}
+
+const mathFunction = x => x**2
