e931441ba5a87dc4e1acb1709037c59e6d4236a7
Picasso <urvishp13@gmail.com>
2023-08-26 15:08:40 -0400

    fixed responsive widths of transaction btns

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -2,6 +2,7 @@ import { accounts } from "./accounts.js"
 
 const get = id => document.getElementById(id)
 
+// grab the Accounts section of the page
 const accountsEl = get("accounts")
 // grab the accounts from the accounts.js file
 // write them and their balance to the DOM
@@ -13,7 +14,6 @@ for (const account of accounts) {
         </button>
     `
 }
-
 accountsEl.innerHTML += html
 
 // grab the Spendings section of the page
@@ -21,10 +21,14 @@ const spendingsEl = get("spendings")
 // populate the section with bars from the accounts.js file
     // everytime account is clicked, loop through each spending for the 
     // account and draw it to the DOM
-html = ''
-let spendingBarNum = 0
 accountsEl.querySelectorAll("button")
     .forEach(accountBtn => accountBtn.addEventListener("click", function() {
+        let html = ''
+        let spendingBarNum = 0
+
+        // clear the Spendings section of the previous data (if exists)
+        spendingsEl.innerHTML = '<h2>Spendings</h2>'
+
         const account = accounts[accountBtn.id-1]
 
         for (const spending of account.spendings) {
@@ -38,9 +42,6 @@ accountsEl.querySelectorAll("button")
         spendingsEl.innerHTML += html
 
         setWidthOfBars(document.getElementsByClassName("spendingBar"), account)
-        // reset html to prepare for displaying the spending for the next
-        //      accountBtn click
-        html = ''
     }))
 
 const setWidthOfBars = (spendingBars, account) => {
